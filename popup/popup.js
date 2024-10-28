8ea6b224886b64b9a87327642af98ca32a05a052
trungdangtapcode <tcuong1000@gmail.com>
2024-10-28 23:58:02 +0700

    test hightlighed text

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,9 @@
 const article = document.querySelector('#abstract')
 console.log(article.textContent)
+document.addEventListener('mouseup', () => {
+    
+});
+setInterval(() => {
+    const selectedText = window.getSelection().toString().trim();
+    console.log("Highlighted Text:", selectedText);
+}, 1000);

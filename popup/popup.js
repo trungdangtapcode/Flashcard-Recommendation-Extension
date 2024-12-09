9337f8552e8c9880d50a361d7b1c1d4cbfa4baf6
trungdangtapcode <tcuong1000@gmail.com>
2024-12-09 23:57:40 +0700

    add vite + nest(mongo)

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,8 @@
 const article = document.querySelector('#abstract')
 console.log(article.textContent)
 document.addEventListener('mouseup', () => {
-    
+    const selectedText = window.getSelection().toString().trim();
+    alert(selectedText)
 });
 setInterval(() => {
     const selectedText = window.getSelection().toString().trim();
